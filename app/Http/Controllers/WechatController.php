538b1fc56cfbf84399c64a8d81bad19cd0155557
kasd <540751817@qq.com>
2017-04-13 22:26:31 +0800

    '自己写微信服务器验证'

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -13,10 +13,21 @@ class WechatController extends Controller
      * @author 张皓
      * @return mixed
      */
-    public function serve()
+    public function serve(Request $request)
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
+        $signature = $request->input('signature');
+        $timestamp = $request->input('timestamp');
+        $nonce = $request->input('nonce');
+        $echostr = $request->input('echostr');
+
+        if ($this->checkSignature($signature,$timestamp,$nonce)){
+            echo $echostr;
+        }
+
+        return ;
+        
         $wechat = app('wechat');
 
         $wechat->server->setMessageHandler(function($message){
@@ -26,5 +37,20 @@ class WechatController extends Controller
         Log::info('return response.');
         $response = $wechat->server->serve();
         return $response;
+    }
+
+    private function checkSignature($signature,$timestamp,$nonce)
+    {
+        $token = env('WECHAT_TOKEN');
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
     }
 }

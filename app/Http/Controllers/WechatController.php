374315dacb11dd8cd9d956123ed3f3fc3e094121
kasd <540751817@qq.com>
2017-04-13 22:45:10 +0800

    '自己写微信服务器验证'

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,7 +16,7 @@ class WechatController extends Controller
     public function serve(Request $request)
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-
+        Log::info($request);
         $signature = $request->input('signature');
         $timestamp = $request->input('timestamp');
         $nonce = $request->input('nonce');
